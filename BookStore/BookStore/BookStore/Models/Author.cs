09e193f0dd493f0e1d493d014358193e8d18167c
Bilyana Atanasova <c.coneva@mail.bg>
2021-03-14 17:25:51 +0200

    add exceptions

---
+++ BookStore/BookStore/BookStore/Models/Author.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Models
+{
+    public class Author
+    {
+        private string fristName;
+        private string lastName;
+
+        public Author(int idAuthor, string firstName, string lastName)
+        {
+            this.IdAuthor = idAuthor;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public int IdAuthor { get; private set; }
+        public string  FirstName 
+        {
+            get { return fristName; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Author first name cannot be null or empty!");
+                }
+                fristName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Author last name cannot be null or empty!");
+                }
+            }
+        }
+    }
+}
