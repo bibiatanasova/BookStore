9d97dc6b85e2fe072966d58a480f29999ebd9236
bibiatanasova <78483307+bibiatanasova@users.noreply.github.com>
2021-03-21 19:47:52 +0200

    Update Seller.cs

--- Seller.cs
+++ Seller.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,45 +7,23 @@ namespace BookStore.Models
 {
     public class Seller
     {
-        private string firstName;
-        private string lastName;
+       
 
         public Seller()
         {
 
         }
 
-        public Seller(int idSeller, string firsName, string lastName)
+        public Seller(int id, string firsName, string lastName)
         {
-            this.IdSeller = idSeller;
+            this.Id = id;
             this.FirstName = firsName;
             this.LastName = lastName;
         }
 
-        public int IdSeller { get; set; }
-        public string FirstName 
-        {
-            get { return firstName; }
-            private set
-            {
-                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentNullException("Seller first name cannot be null or empty!");
-                }
-                firstName = value;
-            }
-        }
-        public string LastName 
-        {
-            get { return lastName; }
-            private set
-            {
-                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentNullException("Seller last name cannot be null or empty!");
-                }
-            }
-        }
+        public int Id { get; set; }
+        public string FirstName { get;  set; }
+        public string LastName { get;  set; }
 
     }
 }
