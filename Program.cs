43603592dde28f8ff22ac7300956c82b2a14aaf4
bibiatanasova <78483307+bibiatanasova@users.noreply.github.com>
2021-03-21 19:52:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Controller;
+using BookStore.Models;
+using BookStore.View;
+using System;
+
+namespace BookStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataConnection dataConnection = new DataConnection();
+            BookOperation bookOperation = new BookOperation(dataConnection);
+            Display display = new Display(bookOperation);
+        }
+    }
+}
