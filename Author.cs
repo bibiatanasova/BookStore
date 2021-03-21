277475c70e20f2e9d07dd270430abcc2ae6e21c9
bibiatanasova <78483307+bibiatanasova@users.noreply.github.com>
2021-03-21 19:50:21 +0200

    Update Author.cs

--- Author.cs
+++ Author.cs
@@ -1,25 +1,32 @@
 
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookStore.Models
 {
     public class Author
     {
-        public Author()
+        public Author(int id, string firstName, string lastName)
         {
-
-        }     
-        public Author(int idAuthor, string firstName, string lastName)
-        {
-            this.IdAuthor = idAuthor;
+            this.Id = id;
             this.FirstName = firstName;
             this.LastName = lastName;
         }
+        public Author()
+        {
 
-        public int IdAuthor { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
+        }   
+       
+        public int Id { get;  set; }
+        public string FirstName { get;  set; }
+        public string LastName { get;  set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id} - {FirstName} {LastName}";
+        }
     }
 }
